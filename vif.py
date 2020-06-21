b26a6f2528bfd4be3edb97e99f8a35073fd2ddd8
Shota Yamazaki <sysy5867@gmail.com>
2020-06-22 08:35:13 +0900

    fix

--- vif.py
+++ vif.py
@@ -2,4 +2,4 @@ def get_vifs(model):
   # Note: Please import variance_inflation_factor as vif and pandas as pd before using this function.
   col_num = model.exog.shape[1] 
   vifs = [vif(model.exog, i) for i in range(0, col_num)]
-  pd.DataFrame(vifs, index=model.exog_names, columns=["VIF"])
+  print(pd.DataFrame(vifs, index=model.exog_names, columns=["VIF"]))
