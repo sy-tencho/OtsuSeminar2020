55776ec740a4b7405d79091c3ab9bcea54d4bc4f
Shota Yamazaki <sysy5867@gmail.com>
2020-06-22 10:23:16 +0900

    add comment

--- vif.py
+++ vif.py
@@ -1,6 +1,6 @@
 def get_vifs(model):
   # Note: Please import variance_inflation_factor as vif and pandas as pd before using this function.
-  #        model(parameter)
+  #       model(parameter) should be an instance of statsmodels.OLS class
   col_num = model.exog.shape[1] 
   vifs = [vif(model.exog, i) for i in range(0, col_num)]
   print(pd.DataFrame(vifs, index=model.exog_names, columns=["VIF"]))
